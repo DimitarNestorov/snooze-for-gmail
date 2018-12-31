a31dd5badf1a434681bb5cede4e84c895b944655
Dimitar Nestorov <opensource@dimitarnestorov.com>
2018-12-31 06:16:32 +0200

    Main view UI

---
+++ packages/addon/src/index.ts
@@ -0,0 +1,42 @@
+import { getTimeDropdown, getFolderDropdown, getFromDropdown } from './dropdowns'
+import { getDefaults } from './defaults'
+
+type GmailEvent = {
+	messageMetadata: {
+		accessToken: string,
+		messageId: string,
+	},
+}
+
+declare var global: any
+
+global.main = function main(event: GmailEvent) {
+	const accessToken = event.messageMetadata.accessToken
+	GmailApp.setCurrentMessageAccessToken(accessToken)
+
+	const cards: GoogleAppsScript.Card.Card[] = []
+
+	const card = CardService.newCardBuilder()
+	card.setHeader(CardService.newCardHeader().setTitle('Snooze'))
+
+	const defaults = getDefaults()
+	const button = CardService.newTextButton().setText('Snooze').setOnClickAction(
+		CardService.newAction().setFunctionName('handleSnoozeClick')
+	)
+
+	const section = CardService.newCardSection()
+		.addWidget(getTimeDropdown('Snooze for', defaults.days))
+		.addWidget(getFromDropdown(event.messageMetadata.messageId, defaults.from))
+		.addWidget(getFolderDropdown('Move emails to', defaults.folder))
+		.addWidget(CardService.newButtonSet().addButton(button))
+
+	card.addSection(section)
+
+	cards.push(card.build())
+
+	return cards
+}
+
+global.handleSnoozeClick = function handleSnoozeClick() {
+
+}
---
+++ packages/addon/src/properties.ts
@@ -0,0 +1,1 @@
+export const userProperties = PropertiesService.getUserProperties()
