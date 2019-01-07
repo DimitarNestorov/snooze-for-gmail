f82295a3ab3de3350b11e986b40e32a6e7e3cf3b
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2019-01-07 23:42:46 +0200

    Linked Privacy Policy on homepage

--- packages/hosting/src/index.ts
+++ packages/hosting/src/index.ts
@@ -9,4 +9,10 @@ if (location.pathname === `/${LOGIN_CALLBACK_CLOUD_FUNCTION_NAME}`) {
 	img.height = 100
 	img.alt = "Logo"
 	document.body.appendChild(img)
+	const a = document.createElement('a')
+	a.href = './privacy.html'
+	a.innerText = 'Privacy Policy'
+	const div = document.createElement('div')
+	div.appendChild(a)
+	document.body.appendChild(div)
 }
