1f7f8397f85d6003a28bbe8b2f27b161d737106b
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2019-01-11 18:24:28 +0200

    Simplifying add-on webpack config

--- packages/addon/webpack.config.js
+++ packages/addon/webpack.config.js
@@ -6,25 +6,13 @@ module.exports = {
 	...baseConfig,
 
 	entry: [
-		'./src/appsscript.json',
+		'./loaders/appsscript-loader.js!./src/appsscript.json',
 		// './src/globalThis',
 		'./src/index',
 	],
 	output: {
 		filename: 'index.js',
 		libraryTarget: 'this',
-	},
-	resolveLoader: {
-		modules: [path.join(__dirname, 'loaders'), 'node_modules'],
-	},
-	module: {
-		rules: [
-			...baseConfig.module.rules,
-			{
-				test: /appsscript\.json$/,
-				use: "appsscript-loader",
-			},
-		]
 	},
 	optimization: {
 		minimize: false,
