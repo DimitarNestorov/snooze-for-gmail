59b105288a1cac15f03bb2d29e3f03f42d4cbd0a
Dimitar Nestorov <dimitarnestorov@users.noreply.github.com>
2019-02-09 01:52:01 +0200

    Updating type definitions
    Closes #23 #22 #17

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -6,6 +6,8 @@ export function capitalizeFirstLetter(string: string) {
 	return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
+declare var console: { error: (...args: any[]) => void }
+
 export function logError(error: any) {
 	console.error(error)
 }
